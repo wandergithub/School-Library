056d68acfa990c701b10175d1c9328b2a1762517
wandergithub <wandergonzalez0@hotmail.com>
2022-07-20 11:06:02 -0400

    delete testing code

--- person.rb
+++ person.rb
@@ -29,10 +29,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-  p person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
-  p capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-  p capitalizedTrimmedPerson.correct_name
